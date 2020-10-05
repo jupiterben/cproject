84bd4284f1ba700fa51ba11e0b1ec0d520f5f95c
fub <jupiter.ben@live.com>
2020-10-05 12:53:25 +0800

    temp commit

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -1,2 +1,41 @@
 #define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+//#include <catch.hpp>
+
+#include <unordered_map>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int result = 0;
+        const unordered_map<char, int> charMap;
+        int i = 0, j = 0;
+
+        while (j < s.length())
+        {
+            char c = s.at(j);
+            auto itr = charMap.find(c);
+            if (itr != charMap.end())
+            {
+                int preIndex = itr->second;
+                for (int t = i; t < preIndex + 1; ++t)
+                {
+                    charMap.erase(s.at(t));
+                }
+                i = preIndex + 1;
+            }
+            else
+            {
+                charMap.insert(make_pair(c, j));
+                ++j;
+            }
+            result = max(result, j - i);
+        }
+
+        return result;
+    }
+};
