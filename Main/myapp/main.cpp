c35f1b7dcadc2adb6732df3b9219f8c5cdf2d1ac
fub <jupiter.ben@live.com>
2021-06-15 22:47:39 +0800

    atemp

--- Main/myapp/main.cpp
+++ Main/myapp/main.cpp
@@ -1,15 +1,20 @@
 
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <cmath>
-#include <vector>
-#include <cctype>
-#include <fstream>
-using namespace std;
+
+#include "loopbuilder.h"
 
 int main(int argc, char **argv)
 {
+	LoopBuilder lpb;
+	lpb.addLine(Point2d(0,0), Point2d(0,1));
+	lpb.addLine(Point2d(0, 1.0001), Point2d(1, 1));
+	lpb.addLine(Point2d(0, 1.0001), Point2d(3, 1));
+	lpb.addLine(Point2d(1, 1), Point2d(1, 0));
+	lpb.addLine(Point2d(1, 0), Point2d(0, 0));
+
+
+	lpb.build();
+
+	auto result = lpb.getInnerPaths();
 
   return 1;
 }
--- main/myapp/loopbuilder.h
+++ main/myapp/loopbuilder.h
@@ -6,188 +6,238 @@
 
 struct Point2d
 {
-    double x;
-    double y;
-    Point2d(double _x, double _y) : x(_x), y(_y) {}
+	double x;
+	double y;
+	Point2d(double _x, double _y) : x(_x), y(_y) {}
 };
+typedef std::vector<Point2d> Path;
 
 class Math2d
 {
 public:
-    static double dot(const Point2d &v1, const Point2d &v2)
-    {
-        return v1.x * v2.x + v1.y * v2.y;
-    }
-    static double cross(const Point2d &v1, const Point2d &v2)
-    {
-        return v1.x * v2.y - v1.y * v2.x;
-    }
-    static Point2d sub(const Point2d &v1, const Point2d &v2)
-    {
-        return Point2d(v1.x - v2.x, v1.y - v2.y);
-    }
+	static double dot(const Point2d &v1, const Point2d &v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+	static double cross(const Point2d &v1, const Point2d &v2)
+	{
+		return v1.x * v2.y - v1.y * v2.x;
+	}
+	static Point2d sub(const Point2d &v1, const Point2d &v2)
+	{
+		return Point2d(v1.x - v2.x, v1.y - v2.y);
+	}
 	static double pi() { return std::atan(1) * 4; }
-    static double LinelineCCWAngle(const Point2d &base, const Point2d &p1, const Point2d &p2)
-    {
-        auto v1 = Math2d::sub(p1, base);
+	static double LinelineCCWAngle(const Point2d &base, const Point2d &p1, const Point2d &p2)
+	{
+		auto v1 = Math2d::sub(p1, base);
 		auto v2 = Math2d::sub(p2, base);
 		auto dot = Math2d::dot(v1, v2);
 		auto cross = Math2d::cross(v1, v2);
 		double degree = Math2d::toDegrees(atan2(cross, dot));
-        return fmod(degree + 360,   360);
-    }
+		return fmod(degree + 360, 360);
+	}
+	static double Area(const std::vector<Point2d>& polygon)
+	{
+		double sum = 0;
+		for (size_t j = polygon.size() - 1, i = 0; i < polygon.size(); j = i++) {
+			const auto& p1 = polygon[j];
+			const auto& p2 = polygon[i];
+			sum += (p1.x * p2.y - p1.y * p2.x);
+		}
+		return sum;
+	}
 
 	static double toDegrees(double rad) {
 		return rad * 180 / Math2d::pi();
 	}
+
+	static bool lineLineIntersect(const Point2d& p1, const Point2d& p2, const Point2d& p3, const Point2d& p4, Point2d& out)
+	{
+		double det = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
+		if (det < 0.00000001) return false;
+		double invDet = 1.0 / det;
+		double _tmp1 = (p1.x * p2.y - p1.y * p2.x), _tmp2 = (p3.x * p4.y - p3.y * p4.x);
+		double _x = _tmp1 * (p3.x - p4.x) - (p1.x - p2.x) * _tmp2;
+		double _y = _tmp1 * (p3.y - p4.y) - (p1.y - p2.y) * _tmp2;
+		out.x = _x * invDet;
+		out.y = _y * invDet;
+	}
 };
 
-class HFHalfEdge;
+class HFEdge;
 class HFVertex;
+class HFLoop;
 
 class HFVertex : public Point2d
 {
 public:
+	HFVertex(const Point2d& pos) : Point2d(pos.x, pos.y) {}
+};
+class HFEdge
+{
 public:
-    HFVertex(double _x, double _y) : Point2d(_x, _y) {}
+	HFVertex *from;
+	HFVertex *to;
+
+	HFEdge *next;
+	HFEdge *partner;
+	HFLoop *loop;
+
+	HFEdge(HFVertex *f, HFVertex *t)
+		: from(f), to(t), next(nullptr), partner(nullptr), loop(nullptr)
+	{
+	}
 };
 
 class HFLoop
 {
+	friend class LoopBuilder;
 public:
-    std::vector<HFHalfEdge *> edges;
-};
-
-class HFHalfEdge
-{
-public:
-    HFVertex *from;
-    HFVertex *to;
-
-    HFHalfEdge *next;
-    HFHalfEdge *partner;
-    HFLoop *loop;
-
-    HFHalfEdge(HFVertex *f, HFVertex *t)
-        : from(f), to(t), next(nullptr), partner(nullptr), loop(nullptr)
-    {
-    }
+	Path GetPath()const
+	{
+		Path path;
+		for (size_t i = 0; i < edges.size(); ++i)
+		{
+			const HFEdge* edge = edges[i];
+			path.push_back(Point2d(edge->from->x, edge->from->y));
+		}
+		return path;
+	}
+protected:
+	std::vector<HFEdge *> edges;
 };
 
 class LoopBuilder
 {
 protected:
-    double tolerance;
+	double tolerance;
 
 public:
-    LoopBuilder(double tol)
-    {
-        tolerance = tol;
-    }
+	LoopBuilder(double tol = 0.0001)
+	{
+		tolerance = tol;
+	}
 
-    void addLine(const Point2d &p1, const Point2d &p2)
-    {
-        HFVertex *v1 = _appendVertex(p1.x, p1.y);
-        HFVertex *v2 = _appendVertex(p2.x, p2.y);
-        if (findEdge(v1, v2))
-            return;
+	void addLine(const Point2d &p1, const Point2d &p2)
+	{
+		HFVertex *v1 = _appendVertex(p1);
+		HFVertex *v2 = _appendVertex(p2);
+		if (v1 == v2) return;
+		if (findEdge(v1, v2))
+			return;
 		auto e1 = _appendHalfEdge(v1, v2);
 		auto e2 = _appendHalfEdge(v2, v1);
 		e1->partner = e2;
 		e2->partner = e1;
-    }
+	}
 
 	void build() {
 		for (auto iter = vertexOutHalfEdges.begin(); iter != vertexOutHalfEdges.end(); ++iter)
 		{
 			OutEdgeList& outHalfEdges = iter->second;
 			if (outHalfEdges.empty())continue;
-			auto sorter = [](const OutEdge& e1,  const OutEdge& e2)->bool { return e1.second < e2.second; };
+			auto sorter = [](const OutEdge& e1, const OutEdge& e2)->bool { return e1.second < e2.second; };
 			std::sort(outHalfEdges.begin(), outHalfEdges.end(), sorter);
-			
-			OutEdgeList sortedEdges = outHalfEdges;
-			sortedEdges.push_back(outHalfEdges[0]);
-			for (int i = 0; i < sortedEdges.size()-1; ++i)
+
+			for (size_t j = outHalfEdges.size() - 1, i = 0; i < outHalfEdges.size(); j = i++)
 			{
-				HFHalfEdge* pre = sortedEdges[i].first;
-				HFHalfEdge* next = sortedEdges[i+1].first;
+				HFEdge* pre = outHalfEdges[j].first;
+				HFEdge* next = outHalfEdges[i].first;
 				next->partner->next = pre;
 			}
 		}
 		for (auto iter = edges.begin(); iter != edges.end(); ++iter)
 		{
-			findEdgeLoop(iter->get());
+			_findEdgeLoop(iter->get());
 		}
 	}
 
-	HFLoop* findEdgeLoop(HFHalfEdge* start) {
-		if (start->loop) return start->loop;
-		HFLoop* loop = new HFLoop();
-		loops.push_back(std::unique_ptr<HFLoop>(loop));
-		HFHalfEdge* iter = start;
-		do 
+	std::vector<Path> getInnerPaths()
+	{
+		std::vector<Path> ccwPath;
+		for (auto iter = loops.begin(); iter != loops.end(); ++iter)
 		{
-			if (iter->loop) break;
-			iter->loop = loop;
-			loop->edges.push_back(iter);
-			iter = iter->next;
-		} while (iter && iter!= start);
-		return start->loop;
+			const HFLoop* loop = iter->get();
+			auto path = loop->GetPath();
+			if (Math2d::Area(path) > 0) {
+				ccwPath.push_back(path);
+			}
+		}
+		return ccwPath;
 	}
 
-	public:
-    std::vector<std::unique_ptr<HFVertex>> vertices;
-    std::vector<std::unique_ptr<HFHalfEdge>> edges;
-    std::vector<std::unique_ptr<HFLoop>> loops;
+public:
+	std::vector<std::unique_ptr<HFVertex>> vertices;
+	std::vector<std::unique_ptr<HFEdge>> edges;
+	std::vector<std::unique_ptr<HFLoop>> loops;
 protected:
-	typedef std::pair<HFHalfEdge*, double> OutEdge;
+	typedef std::pair<HFEdge*, double> OutEdge;
 	typedef std::vector<OutEdge> OutEdgeList;
-    std::map<HFVertex *, OutEdgeList> vertexOutHalfEdges;
+	std::map<HFVertex *, OutEdgeList> vertexOutHalfEdges;
 
 private:
-    HFVertex *_appendVertex(double x, double y)
-    {
-        for (auto itr = vertices.begin(); itr != vertices.end(); ++itr)
-        {
-            if (IsSamePoint(itr->get(), x, y))
-                return itr->get();
-        }
-        HFVertex *ret = new HFVertex(x, y);
-        vertices.push_back(std::unique_ptr<HFVertex>(ret));
-        return ret;
-    }
-    bool IsSamePoint(HFVertex *vertex, double x, double y)
-    {
-        double diffX = vertex->x - x;
-        double diffY = vertex->y - y;
-        return sqrt(diffX * diffX + diffY * diffY) < tolerance;
-    }
+	HFVertex *_appendVertex(const Point2d& pos)
+	{
+		for (auto itr = vertices.begin(); itr != vertices.end(); ++itr)
+		{
+			if (IsSamePoint(itr->get(), pos))
+				return itr->get();
+		}
+		HFVertex *ret = new HFVertex(pos);
+		vertices.push_back(std::unique_ptr<HFVertex>(ret));
+		return ret;
+	}
+	bool IsSamePoint(HFVertex *vertex, const Point2d& pos)
+	{
+		double diffX = vertex->x - pos.x;
+		double diffY = vertex->y - pos.y;
+		return sqrt(diffX * diffX + diffY * diffY) < tolerance;
+	}
 
-    HFHalfEdge *findEdge(HFVertex *v1, HFVertex *v2)
-    {
-        for (auto iter = edges.begin(); iter != edges.end(); ++iter)
-        {
-            HFHalfEdge *edge = (*iter).get();
-            if (edge->from == v1 && edge->to == v2)
-                return edge;
-        }
-        return nullptr;
-    }
+	HFEdge *findEdge(HFVertex *v1, HFVertex *v2)
+	{
+		for (auto iter = edges.begin(); iter != edges.end(); ++iter)
+		{
+			HFEdge *edge = (*iter).get();
+			if (edge->from == v1 && edge->to == v2)
+				return edge;
+		}
+		return nullptr;
+	}
 
-	HFHalfEdge* _appendHalfEdge(HFVertex *from, HFVertex *to)
-    {
-		HFHalfEdge* edge = new HFHalfEdge(from, to);
+	HFEdge* _appendHalfEdge(HFVertex *from, HFVertex *to)
+	{
+		HFEdge* edge = new HFEdge(from, to);
 		auto iter = vertexOutHalfEdges.find(from);
 		if (iter == vertexOutHalfEdges.end()) {
-			vertexOutHalfEdges[from] = OutEdgeList({std::make_pair(edge, 0)});
+			vertexOutHalfEdges[from] = OutEdgeList({ std::make_pair(edge, 0) });
 		}
 		else
 		{
-			HFHalfEdge* base = iter->second[0].first;
+			HFEdge* base = iter->second[0].first;
 			double turnAngle = Math2d::LinelineCCWAngle(*from, *base->to, *to);
 			iter->second.push_back(std::make_pair(edge, turnAngle));
 		}
-		edges.push_back(std::unique_ptr<HFHalfEdge>(edge));
+		edges.push_back(std::unique_ptr<HFEdge>(edge));
 		return edge;
-    }
+	}
+
+	HFLoop* _findEdgeLoop(HFEdge* start) {
+		if (start->loop) return start->loop;
+		HFLoop* loop = new HFLoop();
+		loops.push_back(std::unique_ptr<HFLoop>(loop));
+		HFEdge* iter = start;
+		do
+		{
+			if (iter->loop)
+			{
+				throw "should not happen!";
+			}
+			iter->loop = loop;
+			loop->edges.push_back(iter);
+			iter = iter->next;
+		} while (iter && iter != start);
+		return start->loop;
+	}
 };
