4ff70e4fc08f6901f8e78cdfcb4e691e11329bb9
fub <jupiter.ben@live.com>
2021-06-23 11:41:57 +0800

    remove temp code

--- Main/myapp/main.cpp
+++ Main/myapp/main.cpp
@@ -1,20 +1,5 @@
 
-
-#include "loopbuilder.h"
-
 int main(int argc, char **argv)
 {
-	LoopBuilder lpb;
-	lpb.addLine(Point2d(0,0), Point2d(0,1));
-	lpb.addLine(Point2d(0, 1.0001), Point2d(1, 1));
-	lpb.addLine(Point2d(0, 1.0001), Point2d(3, 1));
-	lpb.addLine(Point2d(1, 1), Point2d(1, 0));
-	lpb.addLine(Point2d(1, 0), Point2d(0, 0));
-
-
-	lpb.build();
-
-	auto result = lpb.getInnerPaths();
-
-  return 1;
+	return 1;
 }
--- main/myapp/loopbuilder.cpp
+++
@@ -1,4 +0,0 @@
-
-
-#include "loopbuilder.h"
-
