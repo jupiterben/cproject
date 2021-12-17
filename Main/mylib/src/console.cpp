4850636422524dbeb10e5953c0c6f821b467748b
fub <jupiter.ben@live.com>
2021-12-17 12:23:14 +0800

    add codes

--- Main/mylib/src/console.cpp
+++ Main/mylib/src/console.cpp
@@ -5,20 +5,18 @@
 #include "c17.hpp"
 
 // https://stackoverflow.com/questions/7232710/convert-between-string-u16string-u32string
-std::string U32toUTF8()
+// https://stackoverflow.com/questions/32055357/visual-studio-c-2015-stdcodecvt-with-char16-t-or-char32-t
+// https://stackoverflow.com/questions/1660492/utf-8-output-on-windows-console
+std::string U32toUTF8(const std::u32string& source)
 {
-    std::string result;
-    std::codecvt_utf8<T::value_type>
-        std::wstring_convert<codecvt_utf8_utf16<T>, T> convertor;
-    result = convertor.to_bytes(source);
-    return result;
-    
+    typedef std::codecvt_utf8<char32_t> convert_type;
+    return std::wstring_convert<convert_type, char32_t>{}.to_bytes(source);
 }
 
 
 Console console;
 void Console::log(var& v)const
 {
-	std::cout << c17::toUTF8(v.toString().cStr()) << std::endl;
+    std::cout << U32toUTF8(v.toString().cStr()) << std::endl;
     //std::wcout << v << std::endl;
 }
